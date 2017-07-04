f653c8ce57ac1286ac51c2ca45d487c5f05fbcf6
heejun8609 <heejun8609@gmail.com>
2017-07-04 18:10:44 +0900

    DatePicker

--- Delusion/urls.py
+++ Delusion/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     url(r'^word_table/?', views.word_table, name='word_table'),
     url(r'^word_2_vec/?', views.word_2_vec, name='word_2_vec'),
     url(r'^search/?', views.simple_list, name='search'),
-    url(r'^test/?', views.test, name='test'),
+    # url(r'^test/?', views.test, name='test'),
 
 ]
